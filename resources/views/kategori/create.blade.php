f6eb9e3699497a11fa11e700f6484c203a0a00ee
anasnh12 <anasnurhidayat70@gmail.com>
2024-10-03 22:31:55 +0700

    last update

--- resources/views/kategori/create.blade.php
+++ resources/views/kategori/create.blade.php
@@ -1,33 +1,46 @@
 @extends('layouts.template')
 
 @section('content')
-<div class="card card-outline card-primary">
-    <div class="card-header">
-        <h3 class="card-title">Tambah Kategori</h3>
+    <div class="card card-outline card-primary">
+        <div class="card-header">
+            <h3 class="card-title">{{ $page->title }}</h3>
+            <div class="card-tools"></div>
+        </div>
+        <div class="card-body">
+            <form method="POST" action="{{ url('kategori') }}" class="form-horizontal">
+                @csrf
+                <div class="form-group row">
+                    <label class="col-1 control-label col-form-label">Kategori kode</label>
+                    <div class="col-11">
+                        <input type="text" class="form-control" id="kategori_kode" name="kategori_kode"
+                            value="{{ old('kategori_kode') }}" required>
+                        @error('kategori_kode')
+                            <small class="form-text text-danger">{{ $message }}</small>
+                        @enderror
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-1 control-label col-form-label">Kategori nama</label>
+                    <div class="col-11">
+                        <input type="text" class="form-control" id="kategori_nama" name="kategori_nama" value="{{ old('kategori_nama') }}"
+                            required>
+                        @error('kategori_nama')
+                            <small class="form-text text-danger">{{ $message }}</small>
+                        @enderror
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-1 control-label col-form-label"></label>
+                    <div class="col-11">
+                        <button type="submit" class="btn btn-primary btn-sm">Simpan</button>
+                        <a class="btn btn-sm btn-default ml-1" href="{{ url('kategori') }}">Kembali</a>
+                    </div>
+                </div>
+            </form>
+        </div>
     </div>
-    <div class="card-body">
-        <form method="POST" action="{{ url('kategori') }}">
-            @csrf
-            
-            <div class="form-group">
-                <label for="kode_kategori">Kode Kategori</label>
-                <input type="text" class="form-control" id="kode_kategori" name="kode_kategori" value="{{ old('kode_kategori') }}" required>
-                @error('kode_kategori')
-                    <small class="form-text text-danger">{{ $message }}</small>
-                @enderror
-            </div>
-            
-            <div class="form-group">
-                <label for="nama_kategori">Nama Kategori</label>
-                <input type="text" class="form-control" id="nama_kategori" name="nama_kategori" value="{{ old('nama_kategori') }}" required>
-                @error('nama_kategori')
-                    <small class="form-text text-danger">{{ $message }}</small>
-                @enderror
-            </div>
-
-            <button type="submit" class="btn btn-primary">Simpan</button>
-            <a href="{{ url('kategori') }}" class="btn btn-default">Kembali</a>
-        </form>
-    </div>
-</div>
 @endsection
+@push('css')
+@endpush
+@push('js')
+@endpush
--- resources/views/kategori/edit.blade.php
+++ resources/views/kategori/edit.blade.php
@@ -1,34 +1,58 @@
 @extends('layouts.template')
 
 @section('content')
-<div class="card card-outline card-primary">
-    <div class="card-header">
-        <h3 class="card-title">Edit Kategori</h3>
+    <div class="card card-outline card-primary">
+        <div class="card-header">
+            <h3 class="card-title">{{ $page->title }}</h3>
+            <div class="card-tools"></div>
+        </div>
+        <div class="card-body">
+            @empty($kategori)
+                <div class="alert alert-danger alert-dismissible">
+                    <h5><i class="icon fas fa-ban"></i> Kesalahan!</h5>
+                    Data yang Anda cari tidak ditemukan.
+                </div>
+                <a href="{{ url('kategori') }}" class="btn btn-sm btn-default mt-2">Kembali</a>
+            @else
+                <form method="POST" action="{{ url('/kategori/' . $kategori->kategori_id) }}" class="form
+horizontal">
+                    @csrf
+                    {!! method_field('PUT') !!} <!-- tambahkan baris ini untuk proses edit yang butuh
+                        method PUT -->
+                    <div class="form-group row">
+                        <label class="col-1 control-label col-form-label">Kategori Kode</label>
+                        <div class="col-11">
+                            <input type="text" class="form-control" id="kategori_kode" name="kategori_kode"
+                                value="{{ old('kategori_kode', $kategori->kategori_kode) }}" required>
+                            @error('kategori_kode')
+                                <small class="form-text text-danger">{{ $message }}</small>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-1 control-label col-form-label">Kategori Nama</label>
+                        <div class="col-11">
+                            <input type="text" class="form-control" id="kategori_nama" name="kategori_nama"
+                                value="{{ old('kategori_nama', $kategori->kategori_nama) }}" required>
+                            @error('kategori_nama')
+                                <small class="form-text text-danger">{{ $message }}</small>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-1 control-label col-form-label"></label>
+                        <div class="col-11">
+                            <button type="submit" class="btn btn-primary btn-sm">Simpan</button>
+                            <a class="btn btn-sm btn-default ml-1" href="{{ url('kategori') }}">Kembali</a>
+                        </div>
+                    </div>
+                </form>
+            @endempty
+        </div>
     </div>
-    <div class="card-body">
-        <form method="POST" action="{{ url('kategori/' . $kategori->id) }}">
-            @csrf
-            @method('PUT')
-            
-            <div class="form-group">
-                <label for="kode_kategori">Kode Kategori</label>
-                <input type="text" class="form-control" id="kode_kategori" name="kode_kategori" value="{{ old('kode_kategori', $kategori->kode_kategori) }}" required>
-                @error('kode_kategori')
-                    <small class="form-text text-danger">{{ $message }}</small>
-                @enderror
-            </div>
-            
-            <div class="form-group">
-                <label for="nama_kategori">Nama Kategori</label>
-                <input type="text" class="form-control" id="nama_kategori" name="nama_kategori" value="{{ old('nama_kategori', $kategori->nama_kategori) }}" required>
-                @error('nama_kategori')
-                    <small class="form-text text-danger">{{ $message }}</small>
-                @enderror
-            </div>
-
-            <button type="submit" class="btn btn-primary">Update</button>
-            <a href="{{ url('kategori') }}" class="btn btn-default">Kembali</a>
-        </form>
-    </div>
-</div>
 @endsection
+
+@push('css')
+@endpush
+@push('js')
+@endpush
