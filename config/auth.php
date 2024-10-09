5104e4b59537665f81f76aa2b8676fab3b32889a
anasnh12 <anasnurhidayat70@gmail.com>
2024-10-09 09:40:24 +0700

    JS7-Pratikum 1– Implementasi Authentication

--- config/auth.php
+++ config/auth.php
@@ -62,9 +62,9 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\Models\User::class,
+            'model' => App\Models\UserModel::class,
         ],
-
+        
         // 'users' => [
         //     'driver' => 'database',
         //     'table' => 'users',
