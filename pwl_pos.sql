b32ed8e7e6d6bc25f261d740bc5536d9d67c80a3
anasnh12 <anasnurhidayat70@gmail.com>
2024-10-14 21:53:07 +0700

    update database

--- pwl_pos.sql
+++ pwl_pos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Oct 14, 2024 at 02:48 PM
+-- Generation Time: Oct 14, 2024 at 02:51 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
