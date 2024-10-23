05fe44177f4e5d95ee6d08f3819179508e3edc82
anasnh12 <anasnurhidayat70@gmail.com>
2024-10-23 20:03:26 +0700

    database

--- pwl_pos.sql
+++ pwl_pos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Oct 14, 2024 at 02:51 PM
+-- Generation Time: Oct 23, 2024 at 01:02 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -76,7 +76,10 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (23, '2024_09_09_041834_m_kategori_table', 15),
 (24, '2024_09_09_041851_m_supplier_table', 15),
 (25, '2024_09_09_044054_m_barang_table', 15),
-(26, '2024_09_09_051849_t_stok_table', 15);
+(26, '2024_09_09_051849_t_stok_table', 15),
+(27, '2024_10_20_152149_add_foto_to_m_user_table', 16),
+(28, '2024_10_20_153333_create_t_penjualan_table', 17),
+(29, '2024_10_20_153550_create_t_penjualan_detail_table', 18);
 
 -- --------------------------------------------------------
 
@@ -109,7 +112,12 @@ INSERT INTO `m_barang` (`barang_id`, `kategori_id`, `barang_kode`, `barang_nama`
 (7, 4, 'BAY-001', 'Susu SGM Coklat 900gr', 92500, 95000, NULL, NULL),
 (8, 4, 'BAY-002', 'Popok Mamy Poko', 56000, 58000, NULL, NULL),
 (9, 5, 'MNM-001', 'Aqua 600ml', 3700, 4500, NULL, NULL),
-(10, 5, 'MNM-002', 'Le Mineral', 3500, 4000, NULL, NULL);
+(10, 5, 'MNM-002', 'Le Mineral', 3500, 4000, NULL, NULL),
+(11, 1, 'SBK-003', 'Telur Omega (10 butir)', 22000, 25000, '2024-10-14 08:13:45', NULL),
+(12, 2, 'SNK-003', 'Sari Roti', 11500, 12500, '2024-10-14 08:13:45', NULL),
+(13, 3, 'MND-003', 'Shampoo Pantene', 17500, 18500, '2024-10-14 08:13:45', NULL),
+(14, 4, 'BAY-003', 'Baju Bayi 2th', 89000, 92500, '2024-10-14 08:13:45', NULL),
+(15, 5, 'MNM-003', 'Cle 600mL', 3750, 4300, '2024-10-14 08:13:45', NULL);
 
 -- --------------------------------------------------------
 
@@ -159,7 +167,8 @@ INSERT INTO `m_level` (`level_id`, `level_kode`, `level_nama`, `created_at`, `up
 (2, 'MNG', 'Manager', NULL, NULL),
 (3, 'STF', 'Staff', NULL, '2024-10-07 18:21:58'),
 (4, 'KSR', 'kasir', '2024-09-12 10:08:02', '2024-10-07 18:20:47'),
-(9, 'BRH', 'buruh', '2024-09-29 06:50:48', '2024-09-29 06:50:48');
+(9, 'BRH', 'buruh', '2024-09-29 06:50:48', '2024-09-29 06:50:48'),
+(14, 'HRD', 'Manager HRD', '2024-10-23 02:50:33', NULL);
 
 -- --------------------------------------------------------
 
@@ -198,6 +207,7 @@ CREATE TABLE `m_user` (
   `level_id` bigint UNSIGNED NOT NULL,
   `username` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `nama` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `foto` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
@@ -207,17 +217,30 @@ CREATE TABLE `m_user` (
 -- Dumping data for table `m_user`
 --
 
-INSERT INTO `m_user` (`user_id`, `level_id`, `username`, `nama`, `password`, `created_at`, `updated_at`) VALUES
-(1, 1, 'admin', 'Administrator', '$2y$12$VCW2ORmzHCRx0BW0B.S4Vu0seP652QJ4pF6FGqbbQ1y1Ap2ul8pbC', NULL, NULL),
-(2, 2, 'manager', 'Manager', '$2y$12$w88py8oYA9a4o5mGNbrJ6.A5M03xJzA8IIQ0E9zb0fuDwgbVhLdh6', NULL, NULL),
-(3, 3, 'staff', 'Staff/Kasir', '$2y$12$Nyfa1waGVWPaMSi9H3YL8edgbeZY.PelTevOs0xKP5nHry6TLcWhG', NULL, '2024-10-07 18:28:05'),
-(9, 4, 'Customer-1', 'Pelanggan Pertama', '$2y$12$cAGQ1L6pxwvxbSu.d2Yfh.GExCF.VIaczpS2RUneIlCPQHor94ag6', NULL, '2024-09-12 03:21:50'),
-(19, 2, 'manager_dua', 'Manager 2', '$2y$12$c7dGA.aGfAPshbxVOSWSn.0FV1XORhST8RTb8fOzdXfax5XKxvRfa', '2024-09-17 06:48:30', '2024-09-17 06:48:30'),
-(21, 2, 'manager22', 'Manager Dua Dua', '$2y$12$ntkmb9xvW9m8cO9QHpmA4uDgQmcRCHcR9zjrpIB94NUeAq8p1Z6zC', '2024-09-17 07:52:44', '2024-09-17 07:52:44'),
-(22, 2, 'manager33', 'Manager Tiga Tiga', '$2y$12$EHEripNttWJbEuKhUvYL4.5r8C.klQJ28A88qiFXywvap4Wi.PFSW', '2024-09-17 07:57:56', '2024-09-17 07:57:56'),
-(23, 2, 'manager56', 'Manager55', '$2y$12$O56KOvyWdswfiErsRrssZOm1/b92XSgbv4J7MJHwTI.iy/Vg..ekW', '2024-09-17 08:02:44', '2024-09-17 08:02:44'),
-(24, 2, 'manager12', 'Manager111', '$2y$12$bjpOKUv3F5bnimubUlUuEuz56NWZ01uciFmYjmXddCn6m7BGZBLk6', '2024-09-17 08:04:25', '2024-10-03 08:13:54'),
-(43, 2, 'manager100', 'anas', '$2y$12$ct7tMpRv1r9TvuxcssUR..Q0GaTx6gVoVJd6Bw/Eh/a06rEE6tKWC', '2024-10-07 18:53:45', '2024-10-07 18:53:45');
+INSERT INTO `m_user` (`user_id`, `level_id`, `username`, `nama`, `foto`, `password`, `created_at`, `updated_at`) VALUES
+(1, 1, 'admin', 'admin1', 'adminlte/dist/img/1729678353.jpeg', '$2y$12$Ra38Fy1/EsFM6/jQRa/22uENmylTi25V5SZhip4PtxL9fOXHqEek6', NULL, '2024-10-23 03:12:33'),
+(2, 2, 'manager', 'Manager', 'adminlte/dist/img/1729523416.png', '$2y$12$w88py8oYA9a4o5mGNbrJ6.A5M03xJzA8IIQ0E9zb0fuDwgbVhLdh6', NULL, '2024-10-21 08:10:16'),
+(3, 3, 'staff', 'Staff/Kasir', 'adminlte/dist/img/1729503449.png', '$2y$12$ZftWHSnLjuK5V5yBe.UF8ejq7Ounz9dHao47YtzmopMVjjtLbW5CG', NULL, '2024-10-21 02:37:30'),
+(9, 4, 'Customer-1', 'Pelanggan Pertama', 'adminlte/dist/img/default-foto.jpg', '$2y$12$cAGQ1L6pxwvxbSu.d2Yfh.GExCF.VIaczpS2RUneIlCPQHor94ag6', NULL, '2024-09-12 03:21:50'),
+(19, 2, 'manager_dua', 'Manager 2', 'adminlte/dist/img/default-foto.jpg', '$2y$12$c7dGA.aGfAPshbxVOSWSn.0FV1XORhST8RTb8fOzdXfax5XKxvRfa', '2024-09-17 06:48:30', '2024-09-17 06:48:30'),
+(21, 2, 'manager22', 'Manager Dua Dua', 'adminlte/dist/img/default-foto.jpg', '$2y$12$ntkmb9xvW9m8cO9QHpmA4uDgQmcRCHcR9zjrpIB94NUeAq8p1Z6zC', '2024-09-17 07:52:44', '2024-09-17 07:52:44'),
+(22, 2, 'manager33', 'Manager Tiga Tiga', 'adminlte/dist/img/default-foto.jpg', '$2y$12$EHEripNttWJbEuKhUvYL4.5r8C.klQJ28A88qiFXywvap4Wi.PFSW', '2024-09-17 07:57:56', '2024-09-17 07:57:56'),
+(23, 2, 'manager56', 'Manager55', 'adminlte/dist/img/default-foto.jpg', '$2y$12$O56KOvyWdswfiErsRrssZOm1/b92XSgbv4J7MJHwTI.iy/Vg..ekW', '2024-09-17 08:02:44', '2024-09-17 08:02:44'),
+(24, 2, 'manager12', 'Manager111', 'adminlte/dist/img/default-foto.jpg', '$2y$12$bjpOKUv3F5bnimubUlUuEuz56NWZ01uciFmYjmXddCn6m7BGZBLk6', '2024-09-17 08:04:25', '2024-10-03 08:13:54'),
+(43, 2, 'manager100', 'anas', 'adminlte/dist/img/default-foto.jpg', '$2y$12$ct7tMpRv1r9TvuxcssUR..Q0GaTx6gVoVJd6Bw/Eh/a06rEE6tKWC', '2024-10-07 18:53:45', '2024-10-07 18:53:45'),
+(44, 2, 'manager1111', 'manju', 'adminlte/dist/img/default-foto.jpg', '$2y$12$qUNcZ3vvkvmZX/vcMUAyvuix6.Pj3aEghRINdX0XiNVkhyBtFgxgG', '2024-10-20 01:29:13', '2024-10-23 03:15:11'),
+(66, 1, 'admin01', 'anas', 'adminlte/dist/img/1729521414.jpeg', '$2y$12$iAZv/cgMD4XyqwFyGbzo9uq/JnYhjFiBtx.NYKi3lxdQ4g3F1ltrq', '2024-10-21 05:34:27', '2024-10-21 07:36:54'),
+(67, 3, 'staff11', 'keren12', 'adminlte/dist/img/1729515067.png', '$2y$12$JdhepXiciMp89XeUJBUBM.2Fg8RMLdWKzAnQSY.wEyTjGc8az0p1G', '2024-10-21 05:50:41', '2024-10-21 05:51:07'),
+(68, 1, 'admin12', 'admin11', 'adminlte/dist/img/1729676360.jpg', '$2y$12$RCQKAH7YUuE1XbpnV/ChT.iFp0QWli9YhYjWU./mHsUI73eWQ/q1a', '2024-10-21 07:37:50', '2024-10-23 02:39:20'),
+(69, 2, 'manager01', 'manju11', NULL, '$2y$12$3s/hDxAeKX007kX2Xec8CenT1fGwrf.eutyIM6k8ClpiCd/G1BgjW', '2024-10-21 19:47:47', '2024-10-21 19:47:47'),
+(70, 4, 'kasir1', 'vanda', 'adminlte/dist/img/1729589521.png', '$2y$12$hydejiSIrhD/gq/OQ50IHuFzXwOCsV4qKeDdwx6HDThuBQMSrzLae', '2024-10-21 20:02:34', '2024-10-22 02:32:01'),
+(71, 1, 'admin13', 'anasnh', NULL, '$2y$12$vDps/zwGfQOdnyx7CYw8T.lzdfE1psOQl2isTWiFj6SnHRdlJ0TQq', '2024-10-22 06:07:10', '2024-10-22 06:07:10'),
+(72, 2, 'admin14', 'anasnh1', NULL, '$2y$12$NL9FB9EAAiD6Iq0Cb3cKvuDu/u5S7jk3V/.eUfRi8dGb18RNEoh6q', '2024-10-22 06:07:32', '2024-10-22 06:07:32'),
+(73, 2, 'managerrrr', 'anjas', NULL, '$2y$12$rtf9vyYrDgCrxwipmi89oehZ.MMIlpL0T/cqYL9MVOA4UN03xVS2O', '2024-10-22 06:27:03', '2024-10-22 06:27:03'),
+(74, 1, 'admin1000', 'kerennn', NULL, '$2y$12$Vnca0KiYnMNFlIKPjpOX.OqlNz1bY68O3o0bk8LRzm2I5nyrQ2u6y', '2024-10-22 06:29:11', '2024-10-22 06:29:11'),
+(75, 1, 'admin02', 'anas', NULL, '$2y$12$.F8LJw0tK0zT0I.BnOHMy.XXfJ2jIoQbqjYs2KaNqbFeSURS0r0J6', '2024-10-22 20:16:02', '2024-10-22 20:16:02'),
+(76, 1, 'admin03', 'admin03', NULL, '$2y$12$Uykv8Mn91Gz9oaS/jqUdiOWo0f0VCHzbVgKkrqxv/clogvoBbXNXm', '2024-10-23 02:34:52', '2024-10-23 02:34:52'),
+(77, 3, 'staff01', 'anas', NULL, '$2y$12$swgBSCW9g7VruSSLvoTwj.vEVIY9trmvWWR0K61Df9R4Qvf7MvX8m', '2024-10-23 03:11:14', '2024-10-23 03:11:14');
 
 -- --------------------------------------------------------
 
@@ -280,7 +303,7 @@ INSERT INTO `t_penjualan` (`penjualan_id`, `user_id`, `pembeli`, `penjualan_kode
 (7, 1, 'Kevin', 'PJ007', '2024-09-11 10:30:00', NULL, NULL),
 (8, 2, 'Nala', 'PJ008', '2024-09-11 11:00:00', NULL, NULL),
 (9, 3, 'Dicky', 'PJ009', '2024-09-11 11:30:00', NULL, NULL),
-(10, 1, 'Firstya', 'PJ010', '2024-09-11 12:00:00', NULL, NULL);
+(12, 68, 'ANASMANJU', 'PJ011', '2024-10-22 00:00:00', '2024-10-22 09:53:18', '2024-10-22 09:53:18');
 
 -- --------------------------------------------------------
 
@@ -303,36 +326,16 @@ CREATE TABLE `t_penjualan_detail` (
 --
 
 INSERT INTO `t_penjualan_detail` (`detail_id`, `penjualan_id`, `barang_id`, `harga`, `jumlah`, `created_at`, `updated_at`) VALUES
-(1, 1, 1, 5500000, 1, NULL, NULL),
-(2, 1, 2, 1200000, 2, NULL, NULL),
-(3, 1, 3, 850000, 3, NULL, NULL),
-(4, 2, 4, 3500000, 1, NULL, NULL),
-(5, 2, 5, 2700000, 2, NULL, NULL),
-(6, 2, 6, 4500000, 1, NULL, NULL),
-(7, 3, 7, 1750000, 1, NULL, NULL),
-(8, 3, 8, 1100000, 2, NULL, NULL),
-(9, 3, 9, 1400000, 1, NULL, NULL),
-(10, 4, 10, 2200000, 2, NULL, NULL),
-(11, 4, 11, 6500000, 1, NULL, NULL),
-(12, 4, 12, 900000, 2, NULL, NULL),
-(13, 5, 13, 500000, 3, NULL, NULL),
-(14, 5, 14, 4500000, 1, NULL, NULL),
-(15, 5, 15, 3200000, 2, NULL, NULL),
-(16, 6, 1, 5500000, 1, NULL, NULL),
-(17, 6, 2, 1200000, 2, NULL, NULL),
-(18, 6, 3, 850000, 3, NULL, NULL),
-(19, 7, 4, 3500000, 1, NULL, NULL),
-(20, 7, 5, 2700000, 2, NULL, NULL),
-(21, 7, 6, 4500000, 1, NULL, NULL),
-(22, 8, 7, 1750000, 1, NULL, NULL),
-(23, 8, 8, 1100000, 2, NULL, NULL),
-(24, 8, 9, 1400000, 1, NULL, NULL),
-(25, 9, 10, 2200000, 2, NULL, NULL),
-(26, 9, 11, 6500000, 1, NULL, NULL),
-(27, 9, 12, 900000, 2, NULL, NULL),
-(28, 10, 13, 500000, 3, NULL, NULL),
-(29, 10, 14, 4500000, 1, NULL, NULL),
-(30, 10, 15, 3200000, 2, NULL, NULL);
+(1, 1, 1, 68000, 1, NULL, NULL),
+(2, 1, 2, 83000, 2, NULL, NULL),
+(3, 1, 3, 11000, 3, NULL, NULL),
+(4, 2, 4, 7800, 1, NULL, NULL),
+(5, 2, 5, 5000, 2, NULL, NULL),
+(6, 2, 6, 7500, 1, NULL, NULL),
+(7, 3, 7, 95000, 1, NULL, NULL),
+(8, 3, 8, 58000, 2, NULL, NULL),
+(9, 3, 9, 4500, 1, NULL, NULL),
+(10, 4, 10, 4000, 2, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -497,37 +500,37 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT for table `m_barang`
 --
 ALTER TABLE `m_barang`
-  MODIFY `barang_id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `barang_id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `m_kategori`
 --
 ALTER TABLE `m_kategori`
-  MODIFY `kategori_id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `kategori_id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `m_level`
 --
 ALTER TABLE `m_level`
-  MODIFY `level_id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `level_id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `m_supplier`
 --
 ALTER TABLE `m_supplier`
-  MODIFY `supplier_id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `supplier_id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `m_user`
 --
 ALTER TABLE `m_user`
-  MODIFY `user_id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+  MODIFY `user_id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=79;
 
 --
 -- AUTO_INCREMENT for table `personal_access_tokens`
@@ -539,19 +542,19 @@ ALTER TABLE `personal_access_tokens`
 -- AUTO_INCREMENT for table `t_penjualan`
 --
 ALTER TABLE `t_penjualan`
-  MODIFY `penjualan_id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `penjualan_id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `t_penjualan_detail`
 --
 ALTER TABLE `t_penjualan_detail`
-  MODIFY `detail_id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `detail_id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `t_stok`
 --
 ALTER TABLE `t_stok`
-  MODIFY `stok_id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `stok_id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -585,7 +588,7 @@ ALTER TABLE `t_penjualan`
 -- Constraints for table `t_penjualan_detail`
 --
 ALTER TABLE `t_penjualan_detail`
-  ADD CONSTRAINT `t_penjualan_detail_barang_id_foreign` FOREIGN KEY (`barang_id`) REFERENCES `m_barangs` (`barang_id`),
+  ADD CONSTRAINT `t_penjualan_detail_barang_id_foreign` FOREIGN KEY (`barang_id`) REFERENCES `m_barang` (`barang_id`),
   ADD CONSTRAINT `t_penjualan_detail_penjualan_id_foreign` FOREIGN KEY (`penjualan_id`) REFERENCES `t_penjualan` (`penjualan_id`);
 
 --
